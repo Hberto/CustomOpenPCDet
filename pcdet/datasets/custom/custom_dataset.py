0973004c281d3ff149bbb92db48e631d1334e3d3
Herberto Werner <herberto.werner@cml.fraunhofer.de>
2025-03-13 17:42:32 +0100

    custom dataset with train acc

--- pcdet/datasets/custom/custom_dataset.py
+++ pcdet/datasets/custom/custom_dataset.py
@@ -10,7 +10,7 @@ from ..dataset import DatasetTemplate
 
 
 class CustomDataset(DatasetTemplate):
-    def __init__(self, dataset_cfg, class_names, training=True, root_path=None, logger=None):
+    def __init__(self, dataset_cfg, class_names, training=True, root_path=None, logger=None, train_acc=False):
         """
         Args:
             root_path:
@@ -20,7 +20,7 @@ class CustomDataset(DatasetTemplate):
             logger:
         """
         super().__init__(
-            dataset_cfg=dataset_cfg, class_names=class_names, training=training, root_path=root_path, logger=logger
+            dataset_cfg=dataset_cfg, class_names=class_names, training=training, root_path=root_path, logger=logger, train_acc=train_acc,
         )
         self.split = self.dataset_cfg.DATA_SPLIT[self.mode]
 
