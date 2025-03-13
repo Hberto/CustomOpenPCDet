a31289dfcf365b646925de0511e01ea7449c608e
Herberto Werner <herberto.werner@cml.fraunhofer.de>
2025-03-13 16:34:02 +0100

    add train_acc in dataset.py

--- pcdet/datasets/dataset.py
+++ pcdet/datasets/dataset.py
@@ -12,10 +12,11 @@ from .processor.point_feature_encoder import PointFeatureEncoder
 
 
 class DatasetTemplate(torch_data.Dataset):
-    def __init__(self, dataset_cfg=None, class_names=None, training=True, root_path=None, logger=None):
+    def __init__(self, dataset_cfg=None, class_names=None, training=True, root_path=None, logger=None, train_acc=False):
         super().__init__()
         self.dataset_cfg = dataset_cfg
         self.training = training
+        self.train_acc= train_acc
         self.class_names = class_names
         self.logger = logger
         self.root_path = root_path if root_path is not None else Path(self.dataset_cfg.DATA_PATH)
@@ -176,7 +177,7 @@ class DatasetTemplate(torch_data.Dataset):
                 voxel_num_points: optional (num_voxels)
                 ...
         """
-        if self.training:
+        if self.training and not self.train_acc:
             assert 'gt_boxes' in data_dict, 'gt_boxes should be provided for training'
             gt_boxes_mask = np.array([n in self.class_names for n in data_dict['gt_names']], dtype=np.bool_)
             
@@ -210,7 +211,7 @@ class DatasetTemplate(torch_data.Dataset):
             data_dict=data_dict
         )
 
-        if self.training and len(data_dict['gt_boxes']) == 0:
+        if self.training and len(data_dict['gt_boxes']) == 0  and not self.train_acc:
             new_index = np.random.randint(self.__len__())
             return self.__getitem__(new_index)
 
